bb88651334e13834b53f12229b0d4842cac9d9fd
sugipy <hunterpresident@gmail.com>
2020-07-15 15:35:11 +0900

    change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `sugipy.com`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Hikaru Sugita`,
+      summary: `杉田光のブログです。こちらではゆるくまとめていきます。`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
